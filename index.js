00ab07580f121160a158e18e27863481d70d4ab6
natesoum <soumnathun@gmail.com>
2022-08-09 17:36:39 -0400

    Completed generateMarkdown functions & created questions with validators

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        type:'',
-        name:'',
-        message:'',
-        validate: titleName => {
-            if (titleName) {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput) {
                 return true;
             } else {
                 console.log("Please enter a valid title name!");
@@ -16,9 +18,140 @@ const questions = [
         }
     },
     {
-        type:'',
-        name:'',
-        message:'',
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub username?',
+        validate: githubUsername => {
+            if (githubUsername) {
+                return true;
+            } else {
+                console.log("Please enter your GitHub username!")
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'motivation',
+        message: 'What was your motivation for this project?',
+        validate: motivationInput => {
+            if (motivationInput) {
+                return true;
+            } else {
+                console.log('Please enter a valid message for your motivation behind this project!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'why',
+        message: 'Why did you build this project?',
+        validate: whyInput => {
+            if (whyInput) {
+                return true;
+            } else {
+                console.log('Please enter a valid message for your why you built this project!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'problem',
+        message: 'What problem does it solve?',
+        validate: problemInput => {
+            if (problemInput) {
+                return true;
+            } else {
+                console.log('Please enter what problem your project solves!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'learn',
+        message: 'What did you learn from building this project?',
+        validate: learnInput => {
+            if (learnInput) {
+                return true;
+            } else {
+                console.log('Please explain what you learned from building this project!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please explain how to install your application!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide instructions on how to use your project.',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please enter your instructions!')
+                return false;
+            }
+        }
+    },
+    { 
+        type: 'list',
+        name: 'license',
+        message: 'Which license will you use for your project?',
+        choices: ['apache', 'mit', 'mozilla', 'gnu', 'no license']
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please provide how to contribute to your project.',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('Please enter contribution instructions!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide instructions on how to test your application.',
+        validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log('Please enter your test instructions!')
+                return false;
+            }
+        }
     }
 ];
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,106 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'no license') {
+    return `
+    ![badge](https://img.shields.io/badge/license-${license}-brightgreen)
+    `;
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'no license') {
+    return `
+    [${license}](https://choosealicense.com/licenses/${licenses})
+    `;
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'no license') {
+    return `
+    * [License](#license)
+    `;
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ${renderLicenseBadge(data.license)}
+
+  ## Table-of-Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Description](#description)
+  * [Credits](#credits)
+  ${renderLicenseSection(data.license)}
+
+  ## [Description]
+
+  What was your motivation behind this project?
+  ${data.motivation}
+
+  Why did you build this project?
+  ${data.why}
+
+  What problem does this project solve?
+  ${data.problem}
+
+  What did you learn from building this project?
+  ${data.learn}
+
+  ## [Installation]
+
+  What are the steps required to install your project?
+  ${data.installation}
+
+  ## [Usage]
+
+  Provide instructions and examples for use.
+  ${data.usage}
+
+  For more information on how to add screenshots for examples, visit the following website:
+  
+  [Mark Down Tutorial](https://agea.github.io/tutorial.md/)
+
+  ## [Credits]
+
+  If you followed tutorials, went through specific docs or third-party assets. Please list them here. 
+  
+  List your collaborators, if any. Please link their GitHub profile. 
+
+${renderLicenseSection(data.license)}
+
+## [Contributions]
+
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+${data.contribution}
+
+## [Test]
+
+Go the extra mile and write tests for your application. 
+${data.test}
+
+## [Questions]
+
+Please contact me using the following links:
+
+[GitHub](https://github.com/${data.githubUsername})
+
+[Email: ${data.email}](mailto:${data.email})
 
 `;
 }
